a305c64c4b2554fd297aef4d2695e48b11fb46ce
Iryna Boiarska <boyarskairyna32@gmail.com>
2026-05-26 00:30:32 +0300

    homework-6, nextjs

---
+++ components/Pagination/Pagination.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import ReactPaginateModule from 'react-paginate';
+import type { ReactPaginateProps } from 'react-paginate';
+import type { ComponentType } from 'react';
+import css from './Pagination.module.css';
+type ModuleWithDefault<T> = { default: T };
+
+const ReactPaginate = (
+  ReactPaginateModule as unknown as ModuleWithDefault<ComponentType<ReactPaginateProps>>
+).default;
+
+interface PaginationProps {
+  totalPages: number;
+  page: number;
+  onPageChange: (nextPage: number) => void;
+}
+
+export function Pagination({ totalPages, page, onPageChange }: PaginationProps) {
+  return (
+    <ReactPaginate
+      pageCount={totalPages}
+      pageRangeDisplayed={5}
+      marginPagesDisplayed={1}
+      onPageChange={({ selected }) => onPageChange(selected + 1)}
+      forcePage={page - 1}
+      containerClassName={css.pagination}
+      activeClassName={css.active}
+      nextLabel="→"
+      previousLabel="←"
+    />
+  );
+}
